0f5066fc5efbbf2aff4f388bc167eb13e8d52367
Erwan Rossignol <erwan@hotmail.ch>
2025-08-28 02:08:40 +0200

    get back on the project and upgrade packages

--- lib/views/copy_service_page.dart
+++ lib/views/copy_service_page.dart
@@ -1,46 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:table_calendar/table_calendar.dart';
 import '../constants/helper.dart';
 import '../models/appointment.dart';
 import 'main_page.dart';
 
 class CopyServicePage extends StatefulWidget {
-  final Appointment _app;
-  const CopyServicePage({super.key, required Appointment app}) : _app = app;
+  final Appointment app;
+  const CopyServicePage({super.key, required this.app});
 
   @override
-  CopyServicePageState createState() => CopyServicePageState(_app);
+  State<CopyServicePage> createState() => _CopyServicePageState();
 }
 
-class CopyServicePageState extends State<CopyServicePage> {
-  late DateTime _selectedDate;
-
-  CopyServicePageState(Appointment app) {
-    _selectedDate = app.start;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      final DateTime? picked = await showDatePicker(
-        context: context,
-        locale: Get.locale,
-        initialDate: widget._app.start, // Initially selected date
-        firstDate: DateTime(2020), // Earliest date the user can pick
-        lastDate: DateTime(2100), // Latest date the user can pick
-      );
-      if (picked != null) {
-        setState(() => _selectedDate = picked);
-        Helper.copyApp(_selectedDate, widget._app);
-      }
-      Navigator.push(
-          context, MaterialPageRoute(builder: (context) => const MainPage()));
-    });
-  }
+class _CopyServicePageState extends State<CopyServicePage> {
+  Set<DateTime> _selectedDates = {};
+  DateTime _focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ColorScheme colors = Theme.of(context).colorScheme;
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Select dates to copy', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            SizedBox(height: 16),
+            TableCalendar(
+              locale: Get.locale?.languageCode,
+              firstDay: DateTime(2020),
+              lastDay: DateTime(2100),
+              focusedDay: widget.app.start,
+              selectedDayPredicate: (day) => _selectedDates.any((d) => isSameDay(d, day)),
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _focusedDay = focusedDay;
+                  if (_selectedDates.any((d) => isSameDay(d, selectedDay))) {
+                    _selectedDates.removeWhere((d) => isSameDay(d, selectedDay));
+                  } else {
+                    _selectedDates.add(selectedDay);
+                  }
+                });
+              },
+              calendarStyle: CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                  color: colors.primary,
+                  shape: BoxShape.circle,
+                ),
+                todayDecoration: BoxDecoration(
+                  color: colors.secondary,
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: _selectedDates.isEmpty
+                  ? null
+                  : () {
+                      for (var date in _selectedDates) {
+                        Helper.copyApp(date, widget.app);
+                      }
+                      Navigator.of(context).pop();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const MainPage()),
+                      );
+                    },
+              child: Text('Validate'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
